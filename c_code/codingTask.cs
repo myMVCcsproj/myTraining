3d50f09deadb9da9e3089b9d09c3a17b01e1e650
liugayo <liugayo@users.noreply.github.com>
2014-08-08 19:37:26 +0800

    Just Try my code
    Just Try my code, Add to Github.

---
+++ c_code/codingTask.cs
@@ -0,0 +1,18 @@
+using system.io
+
+namespace codingTask
+{
+  public class codingTask
+  {
+    private codingTask()
+    {
+    }
+  
+    public int mainMethod()
+    {
+      Write.Console("OOXX");
+    
+      return 0;
+    }
+  }
+}
